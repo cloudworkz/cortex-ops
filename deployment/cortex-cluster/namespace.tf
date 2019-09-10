d01b9fe00e21b5074d88fe476785ce19aa64b350
Martin Schneppenheim <martin.schneppenheim@rewe-digital.com>
2019-09-10 10:40:02 +0200

    Remove terraform

--- deployment/cortex-cluster/namespace.tf
+++
@@ -1,9 +0,0 @@
-resource "kubernetes_namespace" "monitoring" {
-  metadata {
-    name = "monitoring"
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
--- deployment/cortex-cluster/secrets.tf
+++
@@ -1,9 +0,0 @@
-data "google_kms_secret" "prometheus_bearer_token" {
-  crypto_key = "${data.google_kms_crypto_key.kubernetes.id}"
-  ciphertext = "encrypted_secret_string_goes_here"
-}
-
-data "google_kms_secret" "gateway_jwt_secret" {
-  crypto_key = "${data.google_kms_crypto_key.kubernetes.id}"
-  ciphertext = "encrypted_secret_string_goes_here"
-}
